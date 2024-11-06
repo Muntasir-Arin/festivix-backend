559c437ed4a9351b11a1d6b62142dcd20c9662ac
Muntasir-Arin <muntasirarin@gmail.com>
2024-11-07 03:35:26 +0600

    gravatar added

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,14 +1,27 @@
 require('dotenv').config();
+const crypto = require('crypto'); // Add this import for creating the hash
 const User = require('../models/User');
+
+// Generate Gravatar URL based on email
+function getGravatarUrl(email) {
+    // Prepare the email for hashing
+    const formattedEmail = email.trim().toLowerCase();
+    const hash = crypto.createHash('md5').update(formattedEmail).digest('hex');
+    // Return the Gravatar URL
+    return `https://www.gravatar.com/avatar/${hash}`;
+}
 
 // Get user profile
 exports.getProfile = async (req, res) => {
     try {
-        const user = await User.findById(req.user.id).select('-password');
+        const user = await User.findById(req.user.id).select('-password -_id -verificationToken -__v');
         if (!user) return res.status(404).json({ message: 'User not found' });
 
-        res.json(user);
+        // Add Gravatar URL to user profile
+        const profilePicture = getGravatarUrl(user.email);
+        res.json({ ...user.toObject(), profilePicture });
     } catch (error) {
+        console.error(error);
         res.status(500).json({ message: 'Server error' });
     }
 };
