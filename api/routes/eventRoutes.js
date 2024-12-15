e22bdf6d19d57587087c09bbd7ce85e18979be70
Muntasir Arin <muntasirarin@gmail.com>
2024-12-15 12:47:51 +0600

    ehhe

--- api/routes/eventRoutes.js
+++ api/routes/eventRoutes.js
@@ -4,8 +4,8 @@ const eventController = require('../controllers/eventController');
 const upload = require('../middleware/upload');
 const { verifyToken, checkRole } = require('../middleware/authMiddleware');
 
-router.post('/', verifyToken, checkRole('Manager'), upload, eventController.createEvent);
-router.delete('/:id', verifyToken, checkRole('Manager'), eventController.deleteEvent);
-router.patch('/:id', verifyToken, checkRole('Manager'), upload, eventController.updateEvent);
+router.post('/',  upload, eventController.createEvent);
+router.delete('/:id',  eventController.deleteEvent);
+router.patch('/:id',  upload, eventController.updateEvent);
 
 module.exports = router;
